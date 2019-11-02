d405a02fed2752a7fd6fa6f4893ef5ca4a06cf7d
Chocolatey <chocolatey@realdimensions.net>
2019-11-02 16:06:44 +0000

    AU: 2 updated - rclone rclone.portable [skip ci] https://gist.github.com/228fedee6854c32a34b10e918567a346/ebaeca2dc417f3f8bbd42060cde36399ebf0da71

--- automatic/rclone.portable/tools/chocolateyInstall.ps1
+++ automatic/rclone.portable/tools/chocolateyInstall.ps1
@@ -4,12 +4,12 @@ $toolsDir      = Split-Path $MyInvocation.MyCommand.Definition
 
 $packageArgs = @{
     PackageName  = 'rclone.portable'
-    Url = 'https://github.com/rclone/rclone/releases/download/v1.50.0/rclone-v1.50.0-windows-386.zip'
+    Url = 'https://github.com/rclone/rclone/releases/download/v1.50.1/rclone-v1.50.1-windows-386.zip'
     UnzipLocation = $toolsDir
-    Url64bit = 'https://github.com/rclone/rclone/releases/download/v1.50.0/rclone-v1.50.0-windows-amd64.zip'
-    Checksum = '0b812749ce555f364c0993e7a8f34d09763d147aecea449e53139027b3e804a9'
+    Url64bit = 'https://github.com/rclone/rclone/releases/download/v1.50.1/rclone-v1.50.1-windows-amd64.zip'
+    Checksum = '959b7795c5c2ab20a8b6ca966d27566b44d454e1560a1395a6227673595ae4c3'
     ChecksumType = "sha256"
-    Checksum64 = '2a5a47604d50aaba73beca2cc472b14f8139957af40533e323f20d33a0c228b5'
+    Checksum64 = 'dc991348129a35580226bd7094a1d69999fce8219caaf2cfd4e9e341abcdc0ed'
     Checksum64Type = "sha256"
 }
 
