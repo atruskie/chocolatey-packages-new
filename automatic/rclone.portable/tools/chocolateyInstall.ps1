2ce98db042f4e73b4abbe71c385ac045ddde9773
Chocolatey <chocolatey@realdimensions.net>
2024-03-10 16:06:35 +0000

    AU: 2 updated - rclone rclone.portable [skip ci] https://gist.github.com/atruskie/228fedee6854c32a34b10e918567a346/97b07ec76d1f9e524ad5fdcbd8a56686d5e4edc4

--- automatic/rclone.portable/tools/chocolateyInstall.ps1
+++ automatic/rclone.portable/tools/chocolateyInstall.ps1
@@ -4,8 +4,8 @@ $toolsDir              = Split-Path $MyInvocation.MyCommand.Definition
 $packageArgs = @{
     PackageName    = $env:ChocolateyPackageName
     Destination    = $toolsDir
-    FileFullPath   = Join-Path $toolsDir 'rclone-v1.65.2-windows-386.zip'
-    FileFullPath64 = Join-Path $toolsDir 'rclone-v1.65.2-windows-amd64.zip'
+    FileFullPath   = Join-Path $toolsDir 'rclone-v1.66.0-windows-386.zip'
+    FileFullPath64 = Join-Path $toolsDir 'rclone-v1.66.0-windows-amd64.zip'
 }
 
 #Remove old versions of rclone in the tools directory
